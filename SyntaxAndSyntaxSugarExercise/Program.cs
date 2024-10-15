d4431ff0d18f252b8d2b44ad21bf481cb65c5cbd
Michael Resendez <michael.resendez1@hotmail.com>
2024-10-15 17:04:14 -0500

    Finished the exercise using else / if statements

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,30 @@
 {
     public class Program
     {
+        private static bool response; // Declaring a private static bool variable
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Step 1: Delcare and initialize my variable
+            int answer = 4;
+
+            // Step 2: Determine if the answer is less than 9
+            response = answer < 9; // This sets the resonse to True or False
+
+            // Step 3: Use if-else to create the output message to user
+            string message; // Declare the message variable
+
+            if (response)
+            {
+                message = $"{answer} is less than nine"; // If response is only true
+            }
+            else
+            {
+                message = $"{answer} is greater than or equal to nine"; // If the response is false 
+            }
+
+            // Step 4: Output the message to the console
+            Console.WriteLine(message); // Display the message 
         }
     }
 }
